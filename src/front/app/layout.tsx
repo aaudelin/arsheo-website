abbf6b064c1370513aabd39158229654ef22083b
Aurélien Audelin <aurelien.audelin@gmail.com>
2024-08-13 10:18:20 +0200

    refactor: update prettier

--- src/front/app/layout.tsx
+++ src/front/app/layout.tsx
@@ -1,24 +1,22 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Arsheo | IT as a service",
-    description: "An IT agency with a twist",
+  title: "Arsheo | IT as a service",
+  description: "An IT agency with a twist",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" className="dark">
-        <body className={inter.className}>
-        {children}
-        </body>
-        </html>
-    );
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
