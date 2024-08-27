38176acbbc96c8978675fbd1a1ae0659f0dd06ad
Aurélien Audelin <aurelien.audelin@gmail.com>
2024-08-27 15:21:53 +0200

    feat: add footer

--- src/front/app/page.tsx
+++ src/front/app/page.tsx
@@ -7,6 +7,8 @@ import Tools from "@/components/tools";
 import { Quote } from "lucide-react";
 import Pricing from "@/components/pricing";
 import Contact from "@/components/contact";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -52,10 +54,33 @@ export default function Home() {
       <Pricing />
       <Contact />
 
-      <footer>
-        <p>Mentions légales</p>
-        <p>politique de confidentialité</p>
-        <p>conditions générales d'utilisation</p>
+      <footer className="container border-t-2 py-24 bg-primary/20">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-y-12">
+          <div>
+            <Link
+              href="#"
+              className="flex gap-2 text-lg font-semibold md:text-base"
+            >
+              <Image
+                src="/icon-light.png"
+                alt="Icon Arsheo"
+                width="40"
+                height="40"
+              />
+              <span className="text-3xl tracking-normal">Arsheo</span>
+            </Link>
+            <p className="mt-4">La solution pour scaler votre business</p>
+          </div>
+          <div>
+            <p className="text-lg text-muted-foreground mb-2">Légal</p>
+            <Link href="">
+              <p>Mentions légales</p>
+            </Link>
+            <Link href="">
+              <p>Politique de confidentialité</p>
+            </Link>
+          </div>
+        </div>
       </footer>
     </div>
   );
